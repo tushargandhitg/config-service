475f2327ccd899c4f249d0a69bcdfc375e1921d5
Tushar Gandhi <tgandhi@IT-C02QRA2NG8WP.local>
2019-01-08 12:03:36 +0530

    final commit

--- src/main/java/service/config/ConfigServiceApplication.java
+++ src/main/java/service/config/ConfigServiceApplication.java
@@ -2,11 +2,11 @@ package service.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
